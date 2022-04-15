a998e91f3c343d5b1ac895a75582323b095286aa
Mark Roldan <Mark.Roldan@Hypertherm.com>
2022-04-15 18:37:50 -0400

    Start levels model tests

---
+++ Level-Exporter.Tests/Models/LevelTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using Level_Exporter.Models;
+
+namespace Level_Exporter.Tests.Models
+{
+    [TestFixture]
+    public class LevelTests
+    {
+        [Test]
+        public void NameProperty_ValueIsEmptyString_SetsNameAsLevel()
+        {
+            string name = "";
+            string expectedName = "Level";
+
+            Level level = new Level { Name = name };
+
+            Assert.AreEqual(expectedName, level.Name);
+        }
+
+    }
+}
