689e1b97fe68dc3cba9eba6ca3af8f421244e264
Mark Roldan <Mark.Roldan@Hypertherm.com>
2022-04-15 20:37:08 -0400

    Add vlaid name level test

--- Level-Exporter.Tests/Models/LevelTests.cs
+++ Level-Exporter.Tests/Models/LevelTests.cs
@@ -2,14 +2,41 @@
 using NUnit.Framework;
 using Moq;
 using Level_Exporter.Models;
+using System.Collections.Generic;
 
 namespace Level_Exporter.Tests.Models
 {
+    using System.Linq;
+    using NUnit.Framework.Internal;
+
     [TestFixture]
     public class LevelTests
     {
+        private List<string> invalidNames = new List<string>
+        {
+           "eLee@","$$$!L",
+            "e##LL",
+            "l%%ev",
+            "!$LL*",
+            "Lv:",@"\", "\""
+        };
+
+        private List<string> validNames = new List<string>
+        {
+            "3l3La",
+            "dj9Lj",
+            "sm56e",
+            "mlkdj",
+            "sv4ll",
+            "sf020",
+            "9vj3d",
+            "dedd5",
+            "0je3s",
+            "fvjd6"
+        };
+
         [Test]
-        public void NameProperty_ValueIsEmptyString_SetsNameAsLevel()
+        public void Name_IfValueIsEmptyString_SetsNameAsLevel()
         {
             string name = "";
             string expectedName = "Level";
@@ -19,5 +46,24 @@ namespace Level_Exporter.Tests.Models
             Assert.AreEqual(expectedName, level.Name);
         }
 
+        [Test]
+        [TestCaseSource(nameof(invalidNames))]
+        public void Name__IfValueContainsSymbols_SetsNameAsLevel()
+        {
+            List<Level> names = invalidNames.Select(name => new Level { Name = name }).ToList();
+
+            Assert.That(names, Has.Exactly(names.Count).Matches<Level>(c => c.Name.Equals("Level")),
+                $"Should contain {names.Count} names of Level");
+        }
+
+        [Test]
+        [TestCaseSource(nameof(validNames))]
+        public void Name_IfValidValue_SetsNameAsValue()
+        {
+           var levelObjects = invalidNames.Select(name => new Level { Name = name }).ToList();
+           var names = levelObjects.Select(x => x.Name).ToList();
+           
+            CollectionAssert.AreEqual(validNames, names);
+        }
     }
 }
