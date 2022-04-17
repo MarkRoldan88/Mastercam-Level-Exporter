3d0c50c0a39c56890a7c98261bb553f785757c4f
Mark Roldan <Mark.Roldan@Hypertherm.com>
2022-04-16 21:04:36 -0400

    Add cadformat tests

---
+++ Level-Exporter.Tests/Models/CadFormatTests.cs
@@ -0,0 +1,56 @@
+﻿namespace Level_Exporter.Tests.Models
+{
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using Level_Exporter.Models;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CadFormatTests
+    { 
+        [Test]
+        public void FileExtension_SetterShouldPrependValue_WithPeriod()
+        {
+            const string expectedValue = ".stl";
+
+            CadFormat cadFormat = new CadFormat(CadTypes.Stl);
+
+            Assert.AreEqual(expectedValue,cadFormat.FileExtension);
+        }
+
+        [Test]
+        public void FileExtension_ShouldNotContain_UppercaseLetters()
+        {
+            var invalidFormat = new Regex(@"([^a-z])");
+
+
+        }
+
+        [Test]
+        public void Description_StringShouldBe_SpecificFormat()
+        {
+            //Example: "AutoCAD Drawing File (*.stl)"
+            var validFormat = new Regex(@"((^[A-Z\s]{1,})+\w+\s+\({1}\*{1}.{1}([a-z]{1,})\){1}$)", RegexOptions.IgnoreCase);
+            var invalidFormatExamples = new List<string>
+            {
+                "Test (*.stl)sss",
+                "12 (*.stl)",
+                "12 (*.stl)sss",
+                "12 (.stl)",
+                "sss (*.stl)",
+            };
+
+            string actualDescription = new CadFormat(CadTypes.Emcam).Description;
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(validFormat.IsMatch(actualDescription));
+
+                Assert.That(invalidFormatExamples,
+                    Has.Exactly(invalidFormatExamples.Count).Matches<string>(invalidFormat => !validFormat.IsMatch(invalidFormat)),
+                    "Should not contain any valid regex match");
+            });
+
+        }
+    }
+}
