96e34fa0c9e1ff5612385bd0311a7a9c24aeab48
Mark Roldan <Mark.Roldan@Hypertherm.com>
2022-04-18 16:53:44 -0400

    Add levelInfoViewModelTest class,

---
+++ Level-Exporter.Tests/ViewModels/LevelInfoViewModelTests.cs
@@ -0,0 +1,10 @@
+﻿namespace Level_Exporter.Tests.ViewModels
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LevelInfoViewModelTests
+    {
+        //TODO ADD tests for Onreadmastercamlevels command (moq mastercam functionality?)
+    }
+}
