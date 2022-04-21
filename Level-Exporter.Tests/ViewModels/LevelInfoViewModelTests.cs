dc9960aaaa94b39004732f8b8b7ec881593beb25
Mark Roldan <Mark.Roldan@Hypertherm.com>
2022-04-21 12:31:27 -0400

    Add test for read mastercam levels command

--- Level-Exporter.Tests/ViewModels/LevelInfoViewModelTests.cs
+++ Level-Exporter.Tests/ViewModels/LevelInfoViewModelTests.cs
@@ -1,12 +1,13 @@
 ﻿namespace Level_Exporter.Tests.ViewModels
 {
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
     using Level_Exporter.Models;
     using Level_Exporter.ViewModels;
     using Moq;
-    using NUnit.Framework;
     using NSubstitute;
+    using NUnit.Framework;
 
     [TestFixture]
     public class LevelInfoViewModelTests
@@ -18,8 +19,8 @@
             Level levelA = new Level { IsSelected = false };
             Level levelB = new Level { IsSelected = false };
 
-            LevelInfoViewModel.LevelInfoHelper = Substitute.For<ILevelInfo>();
             Mock<ILevelInfo> levelInfoHelperMock = new Mock<ILevelInfo>();
+
             LevelInfoViewModel.LevelInfoHelper = levelInfoHelperMock.Object;
 
             levelInfoHelperMock.SetupGet(x => x.Levels).Returns(new ObservableCollection<Level>
@@ -27,10 +28,7 @@
                 levelA, levelB
             });
 
-            LevelInfoViewModel levelInfoViewModel = new LevelInfoViewModel()
-            {
-                IsSelectAll = true
-            };
+            LevelInfoViewModel levelInfoViewModel = new LevelInfoViewModel { IsSelectAll = true };
 
             // Act
 
@@ -40,6 +38,55 @@
             Assert.That(levelInfoViewModel.Levels.ToList(),
                 Has.All.Matches<Level>(x => x.IsSelected == levelInfoViewModel.IsSelectAll),
                 "All levels.IsSelected property must match isSelectAll");
+        }
+
+        [Test]
+        public void ReadMastercamLevelsCommand_ShouldPopulateLevelsCollection_WithLevels()
+        {
+            //Arrange 
+            var iLevelInfoSub = Substitute.For<ILevelInfo>();
+
+            string expectedNameA = "level5";
+            string expectedNameB = "level10";
+            int expectedCountA = 5;
+            int expectedCountB = 10;
+
+            iLevelInfoSub.GetLevelsWithGeometry().Returns(new Dictionary<int, string>
+            {
+                { expectedCountA, expectedNameA }, {expectedCountB, expectedNameB }
+            });
+
+            iLevelInfoSub.Levels.Returns(new ObservableCollection<Level>());
+            iLevelInfoSub.GetLevelEntityCount(5).Returns(5);
+            iLevelInfoSub.GetLevelEntityCount(10).Returns(10);
+
+            LevelInfoViewModel.LevelInfoHelper = iLevelInfoSub;
+            LevelInfoViewModel levelInfoViewModel = new LevelInfoViewModel();
+
+            //Act
+            levelInfoViewModel.ReadMastercamLevels.Execute(null);
+
+            //Assert
+            iLevelInfoSub.Received().GetLevelEntityCount(5);
+            iLevelInfoSub.Received().GetLevelEntityCount(10);
+
+            Assert.Multiple(() =>
+            {
+                CollectionAssert.IsNotEmpty(levelInfoViewModel.Levels,
+                    "Collection should not be empty if valid information is collected");
+
+                CollectionAssert.AllItemsAreInstancesOfType(levelInfoViewModel.Levels, typeof(Level),
+                    $"All items of collections must be of type {typeof(Level)}");
+
+                Assert.That(levelInfoViewModel.Levels,
+                    Has.One.Matches<Level>(lvl => lvl.Name.Equals(expectedNameA) && lvl.EntityCount == expectedCountA),
+                    "Collection should contain entry with expected property values");
+
+                Assert.That(levelInfoViewModel.Levels,
+                    Has.One.Matches<Level>(lvl => lvl.Name.Equals(expectedNameB) && lvl.EntityCount == expectedCountB),
+                    "Collection should contain entry with expected property values");
+            });
+
         }
 
     }
